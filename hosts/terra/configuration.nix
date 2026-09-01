47c6a13f4d18c7081b4c48b24f289331e029fcf1
lunarythia <63614345+lunarythia@users.noreply.github.com>
2026-08-31 22:27:19 -0700

    Add fcitx5

--- hosts/terra/configuration.nix
+++ hosts/terra/configuration.nix
@@ -134,6 +134,17 @@
     nerd-fonts.jetbrains-mono
   ];
 
+  i18n.inputMethod = {
+    enable = true;
+    type = "fcitx5";
+    fcitx5 = {
+      addons = with pkgs; [
+        fcitx5-chewing
+        fcitx5-gtk
+      ];
+      waylandFrontend = true;
+    };
+  };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
