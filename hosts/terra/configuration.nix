617109077499cbe20abe831506d977c1532dd568
lunarythia <63614345+lunarythia@users.noreply.github.com>
2026-08-31 22:26:48 -0700

    Add bottles for fusion 360

--- hosts/terra/configuration.nix
+++ hosts/terra/configuration.nix
@@ -112,6 +112,7 @@
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
   environment.systemPackages = with pkgs; [
+    (bottles.override { removeWarningPopup = true; })
     nvd
     vim-full # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     nixd # lsp server for Nix
